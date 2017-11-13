bbc70963264436afaa9d486da1465ec16884e6f8
Ali Younessi <ali.younessi.95@gmail.com>
2017-11-13 20:20:13 +0000

    load port names from config file

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import json
 
 # TODO:
 
-# 1. print an error message if configuration file does not exist ............DONE!
-# 2. print error messages if necessary fields don't exist in config file
-
 #############################################################################
 def load_json(file):
     with open(file, "r") as fid:
@@ -18,36 +15,39 @@ def print_json(obj):
      print(json.dumps(obj, indent=4))
 
 #############################################################################
-def generate_code(mod_name, width, height):
+def generate_code(module_name, width, height,port_north,port_south,port_east,port_west):
     # mod_name = "myblock"
     # mod_name = sys.argv[1]
     
-    str_def = "%s r%d (north[%d], east[%d], south[%d], west[%d]);"  #module definition
-    str_con = "assign north[%d] = south[%d];"   #module connections
+    str_def = "%s r%d (%s [%d], %s[%d], %s[%d], %s[%d]);"  #module definition
+    str_con = "assign %s[%d] = %s[%d];"   #module connections
 
     total = width * height     #Area of the module
 
     for n in range(total):
-        print str_def % (mod_name, n, n, n, n, n)
+        print str_def % (module_name, n, port_north, n, port_south ,n, port_east, n,port_west, n)
         #prints all the block deffinitions accourding to the json file 
         #1 string for name of block and 4 integers for the block increment
-
+       
     print "\n//Connections:\n"
 
-    shifts = [width*x for x in range(height-1)] #?????????
+    shifts = [width*x for x in range(height-1)] 
 
     for row_shift in shifts:
         print "// Connector block (shift = %d):" % row_shift
         for m in range(width):
             n = row_shift + m
-            print str_con % (n, n+width)
+            print str_con % (port_north, n, port_south, n+width)
         print "\n"
+##############################################################################
+
+
 ##############################################################################
 def main():
 
     config_file_path = sys.argv[1]      #takes the input from the user and stores it in a variable.
 
-    #USE it is Easier to Ask for Forgiveness than for Permission" (in short: EAFP)
+    #USE: it is Easier to Ask for Forgiveness than for Permission" (in short: EAFP)
 
     try: 
         config = load_json(config_file_path)
@@ -56,13 +56,17 @@ def main():
         sys.exit(1)      #try loading the file if there is an IO error stop and print error message
 
     try:
+        port_north =config ["port_names"]["north"]
+        port_south = config ["port_names"]["south"]
+        port_east = config ["port_names"]["east"]
+        port_west = config ["port_names"]["west"]
         block_name = config["module_name"]  #set the block name to the "module name" entry in the json files
         width, height = map(int, config["dimensions"]) #in the config file set width and height to the 1st and 2nd index of the array "dimensions"
     except:
         print('There was an error in the config file!')
         sys.exit(1)    
 
-    generate_code(block_name, width, height) #feed the "module name" entry in the json file to the generate code function
+    generate_code(block_name, width, height,port_north,port_south,port_east,port_west) #feed the "module name" entry in the json file to the generate code function
     
 
 ##############################################################################
