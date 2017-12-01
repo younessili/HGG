712592ecc3c423f7c12ec9a101e09b7d83212d02
Ali Younessi <ali.younessi.95@gmail.com>
2017-12-01 18:30:48 +0000

    add doc strings and reduce number of functions

---
+++ README.md
@@ -0,0 +1,9 @@
+. Create README in markdown
+
+- see this for example:
+
+https://github.com/gtarawneh/copter/blob/master/README.md
+
+- Read some articles under the "Articles" section of :
+
+https://github.com/matiassingers/awesome-readme
--- main.py
+++ main.py
@@ -3,71 +3,109 @@
 import sys
 import json
 
-#TODO add reading from config file for  port types and the data types for inputs outputs
-
 #############################################################################
 def load_json(file):
+    """Return the json data by opening the file in read mode  """
     with open(file, "r") as json_data:
         return json.load(json_data)   #load the json file in read mode.
+
 #############################################################################
-def check_input(config_file_path):
-     #USE: it is Easier to Ask for Forgiveness than for Permission" (in short: EAFP)
-    try:
-        config = load_json(config_file_path)
-    except IOError:
-        print('There was No such file to open!')
-        sys.exit(1)      #try loading the file if there is an IO error stop and print error message
-    try:
-        block_name = config["module_name"]  #set the block name to the "module name" entry in the json files
-        width, height = map(int, config["dimensions"]) #in the config file set width and height to the 1st and 2nd index of the array "dimensions"
-        port_names = config["port_names"]
-        port_types= config["port_types"]
-        interfaces = config ["interfaces"]
+def generate_code(config_file_path):
+    """generate the module instances and assignments"""
 
 
-        #add line to read the interfaces code from the config file.
+    def check_input(config_file_path):
+        """Return the main file elements by loadding the congfig file. do this after data validation    """
 
-        return (port_names, port_types,interfaces, block_name, width, height)
-    except:
-        print('There was an error in the config file!')
-        sys.exit(1)
-#############################################################################
-def generate_code(module_name, width, height, port_names,port_types,interfaces):
+        try:
+             config = load_json(config_file_path)
+        except IOError:
+            print('There was No such file to open!')
+            sys.exit(1)      #try loading the file if there is an IO error stop and print error message
 
-    total = width * height     #Area of the module
-
-    port_names = port_names.values()
-    port_type =  port_types.values()    #list comprehension
-    # interface = interfaces.items()
-
+        try:
+            module_name = config["module_name"]  #set the block name to the "module name" entry in the json files
+            width, height = map(int, config["dimensions"]) #in the config file set width and height to the 1st and 2nd index of the array "dimensions"
+            port_names = config["port_names"]
+            interfaces = config ["interfaces"]
+        
+        except:
+            print('There was an error in the config file!')
+            sys.exit(1)
+        return (port_names,interfaces, module_name, width, height)  
 
     ######################list of segment definitions#############################
-
+    port_names, interfaces,module_name, width, height = check_input(config_file_path)
+    area = width * height     #Area of the module
     mod_name= "module %s();" #module header defiinition
-    input_exp = "input %s;"
+    input_exp = "  input %s;" 
+    output_exp = "  output %s;"
     str_def = "  %s r%d (%s);"  #module definition here (%s) is %s [%d], %s[%d], %s[%d], %s[%d]
-    str_con = "     assign %s[%d] = %s[%d];"   #module connections
+    str_con = "     assign %s[%d] = %s[%d];"   #module connections#
+
+    ################## nested functions for module interface generation###########
+    def create_port_def(mod_index, port_name, port_width):
+        """return the argument for the module instances based on the number of data lines for each interface. """
+
+        start_bit = port_width * mod_index
+        end_bit = (port_width * (mod_index+1) )- 1    
+
+       ####need to get the start bit of the first itteration and the last bit of the last itteration for each different interface
+
+
+        if start_bit == end_bit:
+            one_bit_def = "%s [%d]" % (port_name, start_bit)
+            return one_bit_def
+        else:
+            multi_bit_def ="%s[%d:%d]" % (port_name, end_bit, start_bit)
+            return multi_bit_def
+
+
+    def create_interface_def(mod_index, interface):
+        """Return a Verilog port declaration string representing the interface."""
+
+        if interface in interfaces:                                                                                                                                                                                                                                                                                                                                                                                                                                 
+
+            signals = interfaces[interface]["signals"]
+            #IO = interfaces[interface]["IO"]
+    
+
+            indexed_keys = []
+            for key, val in signals.iteritems():
+                indexed_key = create_port_def(mod_index, key, val)
+                indexed_keys.append(indexed_key)
+                indexed_keys_exp = ", ".join(indexed_keys)
+            return indexed_keys_exp
+            
+        else:
+            interface_exp = "%s" % (interface)
+            return interface_exp
+
+#   def create_IO_def (interface):
+        """Return the input and output port definitions """
+
+#           IO = interfaces ["TX"]["IO_def"] #WHY IS [INTERFACE]["IO_tYPE"] NOT ALLOWED ?????
+#           interface_IO_exp = []
+#
+#           for key,val in IO.iteritems():
+#             print key
+#             print val
+#            return interface_IO_exp   
 
     ############### prints the module header definition ####################
 
     print mod_name % (module_name) #feed module name to the mod_name string as an argument to fill %s
 
     ############## prints the port_type defiinitions#############
+
     print "//  --------------------input ports----------------------"
-
-
-    print "// %s" % port_names;
-    print "// %s" % port_type;
-    # print interface;
-    print "\n"
-
-
-    port_type = ", ".join(port_type)
-    print  input_exp % (port_type);
-       # print input_exp % (port_names[0],port_names[1],port_names[2]);
-
+    port_names_values = port_names.values()
+    #IO_parts = [create_IO_def(x) for x in port_names_values]   
+    port_def = ", ".join(port_names_values)
+    print input_exp % port_def
+    
     print "//  --------------------output ports----------------------"
-    print "  output my_out;"
+    print output_exp % ", ".join(port_names_values)
 
     #############prints the port_data_type definitions###########
     print "//  --------------------input data types------------------"
@@ -80,49 +118,9 @@ def generate_code(module_name, width, height, port_names,port_types,interfaces):
     ################### prints all the instansiations of the modules given the are ##############
     print "//  --------------------module instancces-----------------"
 
-    def create_port_def(mod_index, port_name, port_width):
-        # example call:
-        # create_port_def(0, "data", 8)
-        # returns:
-        # "data[7:0]"
-        # "data[15:8]"
-        # "data[23:16]"
-
-        start_bit = port_width * mod_index
-        end_bit = port_width * (mod_index+1) - 1
-
-        if start_bit == end_bit:
-            return "%s [%d]" % (port_name, start_bit)
-        else:
-            return "%s[%d:%d]" % (port_name, end_bit, start_bit)
-
-
-    def create_interface_def(mod_index, interface):
-        """Return a Verilog port declaration string representing the interface."""
-
-        if interface in interfaces:
-
-            signals = interfaces[interface]["signals"]
-
-            # indexed_keys = ["%s [%d]" % (key, mod_index) for key in keys]  # e.g. ["req[0]", "ack[0]", "data[0]"]
-            indexed_keys = []
-            for key, val in signals.iteritems():
-                indexed_key = create_port_def(mod_index, key, val)
-                indexed_keys.append(indexed_key)
-
-            return ", ".join(indexed_keys)
-
-        else:
-            return "%s [%d]" % (interface, mod_index)
-
-
-    for n in range(total):
-
-        port_parts = [create_interface_def(n, x) for x in port_names]
-        # port_parts = ["%s [%d]" % (x, n) for x in port_names] # "up [0]", "down[0]", "right[0]"
+    for n in range(area):
+        port_parts = [create_interface_def(n, x) for x in port_names_values]     
         port_def = ", ".join(port_parts)
-
-
         print str_def % (module_name, n, port_def)
         #prints all the block deffinitions accourding to the json file
         #1 string for name of block and 4 integers for the block increment
@@ -131,23 +129,22 @@ def generate_code(module_name, width, height, port_names,port_types,interfaces):
     ########### prints the connections (Assignments) between the module instances #########
 
 
-    print " \n //north to south"
-    port_north = port_names [2]
-    port_south = port_names [3]
+    print " \n //north to south connections"
+
+    port_north = port_names ["north"] 
+    port_south = port_names ["south"]
 
     shifts = [width*x for x in range(height-1)]
-
     for row_shift in shifts:
         print "\n    // Connector block (shift = %d):" % row_shift
         for m in range(width):
             n = row_shift + m
             print str_con % (port_north, n, port_south, n+width)
 
-#####################################################################################
-    print " \n //east to west"
+    print " \n //east to west connections"
 
-    port_west = port_names [0]
-    port_east = port_names [1]
+    port_west = port_names ["west"]
+    port_east = port_names ["east"]
 
     column = [width*x for x in range(height)]
 
@@ -156,20 +153,17 @@ def generate_code(module_name, width, height, port_names,port_types,interfaces):
         for m in range(width-1):
             n = row_shift + m
             print str_con % (port_west, n, port_east, n+1)
-#####################################################################################
 
 
     ## module footer ##
     print "endmodule \n"
 
-
-
 ##############################################################################
 def main():
-
+    """main function"""
     config_file_path = sys.argv[1]      #takes the input from the user and stores it in a variable.
-    port_names, port_types, interfaces,block_name, width, height = check_input(config_file_path)
-    generate_code(block_name, width, height, port_names, port_types,interfaces) #feed the "module name" entry in the json file to the generate code function
+    generate_code(config_file_path) #feed the "module name" entry in the json file to the generate code function
+
 
 ##############################################################################
 
