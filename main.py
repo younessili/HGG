afb6c7ae7b8c7e5ab892239147d3dc260eebd760
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-12-08 15:03:29 +0000

    Change input/output defs to lists

--- main.py
+++ main.py
@@ -26,13 +26,13 @@ def generate_code(config_file_path):
             width, height = map(int, config["dimensions"]) #in the config file set width and height to the 1st and 2nd index of the array "dimensions"
             port_names = config["port_names"]
             interfaces = config ["interfaces"]
-        
+
         except:
             print('There was an error in the config file!')
             sys.exit(1)
-        return (port_names,interfaces, module_name, width, height)  
+        return (port_names,interfaces, module_name, width, height)
 
-     
+
     ######################list of segment definitions#############################
     port_names, interfaces,module_name, width, height = check_input(config_file_path)
     area = width * height     #Area of the module
@@ -48,7 +48,7 @@ def generate_code(config_file_path):
         """return the argument for the module instances based on the number of data lines for each interface. """
 
         start_bit = port_width * mod_index
-        end_bit = (port_width * (mod_index+1) )- 1    
+        end_bit = (port_width * (mod_index+1) )- 1
 
        ####need to get the start bit of the first itteration and the last bit of the last itteration for each different interface
 
@@ -64,10 +64,10 @@ def generate_code(config_file_path):
     def create_interface_def(mod_index, interface):
         """Return a Verilog port declaration string representing the interface."""
 
-        if interface in interfaces:                                                                                                                                                                                                                                                                                                                                                                                                                                 
+        if interface in interfaces:
 
             signals = interfaces[interface]["signals"]
-        
+
 
             indexed_keys = []
             for key, val in signals.iteritems():
@@ -75,7 +75,7 @@ def generate_code(config_file_path):
                 indexed_keys.append(indexed_key)
                 indexed_keys_exp = ", ".join(indexed_keys)
             return indexed_keys_exp
-            
+
         else:
             interface_exp = "%s" % (interface)
             return interface_exp
@@ -85,26 +85,23 @@ def generate_code(config_file_path):
         """Return the input and output port definitions """
 
         IO = interfaces[interface]["IO"]
-        input_exp = "  input %s;" #input expression 
+        input_exp = "  input %s;" #input expression
         output_exp = "  output %s;" #output expression
 
-        for key, val in IO.iteritems():
-            if key == "input":
-                input_expression =  input_exp % val
-                print input_expression 
-            elif key == "output": 
-                output_expression = output_exp % val
-                print output_expression
-            else: 
-                print("error with config fiel IO declaration look for spelling mistakes")
-                sys.exit(1)
-        
- 
+        valid_keys = ["input", "output"]
+
+        valids = [key in valid_keys for key in IO.keys()]
+
+        if not all(valids):
+            print("error with config fiel IO declaration look for spelling mistakes")
+            sys.exit(1)
+
+
     def create_module_assigmnet():
         """create module assignments based on the user prefered assignments."""
         print " \n //north to south connections"
 
-        port_north = port_names ["north"] 
+        port_north = port_names ["north"]
         port_south = port_names ["south"]
 
         shifts = [width*x for x in range(height-1)]
@@ -125,7 +122,7 @@ def generate_code(config_file_path):
             print "\n    // Connector block (shift = %d):" % row_shift
             for m in range(width-1):
                 n = row_shift + m
-                print str_con % (port_west, n, port_east, n+1)     
+                print str_con % (port_west, n, port_east, n+1)
 
 
     def create_block_code():
@@ -136,7 +133,7 @@ def generate_code(config_file_path):
         ###################prints the IO defiinitions#############
 
         print "//  --------------------input/output ports----------------------"
-        IO_parts = [create_IO_def(x) for x in port_names_values]   
+        IO_parts = [create_IO_def(x) for x in port_names_values]
 
         ###################prints the port_data_type definitions###########
         print "//  --------------------input/output data types------------------"
@@ -147,11 +144,11 @@ def generate_code(config_file_path):
         print "//  --------------------module instancces-----------------"
 
         for n in range(area):
-            port_parts = [create_interface_def(n, x) for x in port_names_values]     
+            port_parts = [create_interface_def(n, x) for x in port_names_values]
             port_def = ", ".join(port_parts)
             print str_def % (module_name, n, port_def)
 
-        
+
         ################### prints all the module assignments ##############
         print "\n//  --------------------module assignments-----------------"
         create_module_assigmnet()
@@ -159,7 +156,7 @@ def generate_code(config_file_path):
         ## module footer ##
         print "\n endmodule "
 
-    
+
     create_block_code()
 ##############################################################################
 def main():
