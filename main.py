aacba4ecf87f9d81edda0412937c376f54f70837
Ali Younessi <ali.younessi.95@gmail.com>
2017-12-23 17:48:57 +0000

    simplify config file and remove port names

--- main.py
+++ main.py
@@ -20,27 +20,26 @@ def generate_code(config_file_path):
         except IOError:
             print('There was No such file to open!')
             sys.exit(1)      #try loading the file if there is an IO error stop and print error message
-
         try:
             module_name = config["module_name"]  #set the block name to the "module name" entry in the json files
             width, height = map(int, config["dimensions"]) #in the config file set width and height to the 1st and 2nd index of the array "dimensions"
-            port_names = config["port_names"]
             interfaces = config ["interfaces"]
-
         except:
             print('There was an error in the config file!')
             sys.exit(1)
-        return (port_names,interfaces, module_name, width, height)
+        return (interfaces, module_name, width, height)
 
 
     ######################list of segment definitions#############################
-    port_names, interfaces,module_name, width, height = check_input(config_file_path)
+    interfaces,module_name, width, height = check_input(config_file_path)
     area = width * height     #Area of the module
     mod_name= "module %s();" #module header defiinition
     IO_type_exp = " %s [%d:%d] %s ;" #Input and output type expression
+    input_exp = "  input %s;" #input expression
+    output_exp = "  output %s;" #output expression
     str_def = "  %s r%d (%s);"  #module definition here (%s) is %s [%d], %s[%d], %s[%d], %s[%d]
     str_con = "     assign %s[%d] = %s[%d];"   #module connections#
-    port_names_values = port_names.values()
+    interface_keys = interfaces.keys()
 
 
     ################## nested functions for module interface generation###########
@@ -85,13 +84,17 @@ def generate_code(config_file_path):
         """Return the input and output port definitions """
 
         IO = interfaces[interface]["IO"]
-        input_exp = "  input %s;" #input expression
-        output_exp = "  output %s;" #output expression
+
+        for key, val in IO.iteritems():
+            if key == "input":
+                 input_expression =  input_exp % val
+                 print input_expression 
+            elif key == "output": 
+                output_expression = output_exp % val
+                print output_expression
 
         valid_keys = ["input", "output"]
-
-        valids = [key in valid_keys for key in IO.keys()]
-
+        valids = [key in valid_keys for key in IO.keys()] 
         if not all(valids):
             print("error with config fiel IO declaration look for spelling mistakes")
             sys.exit(1)
@@ -133,7 +136,7 @@ def generate_code(config_file_path):
         ###################prints the IO defiinitions#############
 
         print "//  --------------------input/output ports----------------------"
-        IO_parts = [create_IO_def(x) for x in port_names_values]
+        IO_parts = [create_IO_def(x) for x in interface_keys]
 
         ###################prints the port_data_type definitions###########
         print "//  --------------------input/output data types------------------"
@@ -144,14 +147,15 @@ def generate_code(config_file_path):
         print "//  --------------------module instancces-----------------"
 
         for n in range(area):
-            port_parts = [create_interface_def(n, x) for x in port_names_values]
+            port_parts = [create_interface_def(n, x) for x in interface_keys]
             port_def = ", ".join(port_parts)
             print str_def % (module_name, n, port_def)
 
 
         ################### prints all the module assignments ##############
         print "\n//  --------------------module assignments-----------------"
-        create_module_assigmnet()
+
+        #create_module_assigmnet()
 
         ## module footer ##
         print "\n endmodule "
